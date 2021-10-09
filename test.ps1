78fdf470b731a9ae8d71c4f51addf101050271de
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-10-09 14:57:48 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,13 +1,15 @@
      if (Test-Path "$env:USERPROFILE\Desktop") {  
 
     $desktop_folder = "$env:USERPROFILE\Desktop"
+    '1'
 }
 
 $regedit_desktop_folder = Get-ItemProperty –Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+$regedit_desktop = $regedit_desktop_folder.'{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}'
  
- 
-If (Test-Path $regedit_desktop_folder.'{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}') {
+If (Test-Path $regedit_desktop) {
     $desktop_folder = $regedit_desktop_folder.'{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}'
+    '2'
 }
 
 
