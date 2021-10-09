efee5522da5e18dbb4fec3dc0b3d78f1f6299d76
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-10-09 09:12:12 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -235,7 +235,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 }
 else {
     $regedit_desktop_folder = Get-ItemProperty –Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
-    $desktop_folder = $regedit_desktop_folder.Desktop
+    $desktop_folder = $regedit_desktop_folder.'{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}'
 }
 
 
