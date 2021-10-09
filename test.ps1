1f1a77d31caa16e6e5205d788f306cacecbdcfa6
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-10-09 15:32:19 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,7 +4,7 @@
     '1'
 }
 
-$regedit_desktop_folder = Get-ItemProperty –Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+$regedit_desktop_folder = Get-ItemProperty –Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\"
 $regedit_desktop = $regedit_desktop_folder.'{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}'
  
 If (!(Test-Path "$env:USERPROFILE\Desktop")) {
