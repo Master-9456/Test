f670fb3d00311b36302b443141c287c663ec325c
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-11-07 10:45:35 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -177,25 +177,7 @@ Add-Type -Assembly 'System.IO.Compression.FileSystem'
 $zip = [System.IO.Compression.ZipFile]::Open($zipFilePath, 'read')
 $zip.Entries | Where-Object Name -eq xpui.js | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath\$($_.Name)", $true) }
 # Достаем из архива xpui.zip файлы css
-$zip.Entries | Where-Object Name -eq xpui.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-artist.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-collection-episodes.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-show.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-playlist.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-track-v2.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-collection-songs.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-collection.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-search.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-profile.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-episode.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-view.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-folder.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-desktop-routes-homepage-takeover-ad.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-queue.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-home.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq 330.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-routes-album.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
-$zip.Entries | Where-Object Name -eq xpui-desktop-modals.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
+$zip.Entries | Where-Object Name -like *.css | ForEach-Object { [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$extractPath_removeRTL\$($_.Name)", $true) }
 $zip.Dispose()
 
 # Делает резервную копию xpui.spa
