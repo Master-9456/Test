a8d1ba43db74864f0f6581fe5ba31567e5b405b7
amd64fox <62529699+amd64fox@users.noreply.github.com>
2022-01-15 15:50:28 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -76,7 +76,7 @@ $webClient = New-Object -TypeName System.Net.WebClient
 try {
     $webClient.DownloadFile(
         # Remote file URL
-        "https://github.com/mrpond/BlockTheSpot/relea55ses/latest/download/chrome_elf.zip",
+        "https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip",
         # Local file path
         "$PWD\chrome_elf.zip"
     )
@@ -91,7 +91,7 @@ catch [System.Management.Automation.MethodInvocationException] {
 
         $webClient.DownloadFile(
             # Remote file URL
-            "https://githu66b.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip",
+            "https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip",
             # Local file path
             "$PWD\chrome_elf.zip"
         )
